967d81858ac71077fd948d6b96f6bf3b1fdfcc51
Cristina Ruiz <cruiz731@gmail.com>
2016-07-22 16:35:02 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+var cors = require('cors');
+var bp = require('body-parser');
+var express = require('express');
+var mongodb = require('mongodb');
+var request = require('request');
+var path = require('path');
+
+var url = mongodb://herokuURL
+
