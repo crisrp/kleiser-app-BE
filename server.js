eecaeef716725483426411fb4a3518e585ff77c5
Cristina Ruiz <cruiz731@gmail.com>
2016-07-28 09:09:10 -0400

    commit

--- server.js
+++ server.js
@@ -1,13 +1,3 @@
-// PART 1 require statements
-
-// PART 2 USE statements (cors, bodyparser)
-
-// PART 3 Urls
-
-// PART 4 backend routes
-
-// PART 5 listen on port 3000
-
 var cors = require('cors');
 var bp = require('body-parser');
 var express = require('express');
@@ -17,29 +7,34 @@ var path = require('path');
 
 var app = express(); //**?
 
+var PORT = process.env.PORT || 3000;
+// var PORT = process.env.PORT || 80;
+
 app.use(cors());
-app.use(bp.urlencoded({extended:true}));
+app.use(bp.text({extended:true}));
+// app.use(bp.urlencoded({extended: true}));
 
 var MongoClient = mongodb.MongoClient;
 
 var mongoUrl = 'mongodb://localhost:27017/kleiser';
+// var mongoUrl = 'mongodb://heroku_r2b0d4gm:vni40asof632mi7jgs4pn1sg34@ds029715.mlab.com:29715/heroku_r2b0d4gm/';
 
+var CAMBRIDGE_API_KEY = process.env;
 
-// var PORT = process.env.PORT || 80;
-// for heroku ^
-
+/* default */
 app.get('/', function (req, res) {
   res.send('kleiser app. Hello!');
 });
 
-
+/*get random phrase*/
 app.get('/usefulphrases', function(request, response) {
   MongoClient.connect(mongoUrl, function(err, db){
     if (err) {
       console.log("Unable to connect to the mongodb server. Error: ", err);
     } else {
         var phrasesCollection = db.collection('usefulphrases');
-        phrasesCollection.find().toArray(function(err, result){
+        var recId = Math.floor(Math.random() * 100) + 1;
+        phrasesCollection.find({id: recId}).toArray(function(err, result){
           if (err) {
             console.log("ERROR!", err);
             response.json(result);
@@ -51,10 +46,90 @@ app.get('/usefulphrases', function(request, response) {
             response.json(result);
           }
         }); // end find
-    }; // end else
+
+    } // end else
   }); // end mongoconnect
 }); //end get
 
-app.listen(3000, function() {
-  console.log('App is listening on port 3000!');
+/*show all saved phrases*/
+app.get('/usefulphrases/saved', function(req, res){
+  MongoClient.connect(mongoUrl, function(err, db){
+    if (err) {
+      console.log("Unable to connect to the mongodb server. Error: ", err);
+    } else {
+      var savedPhrases = db.collection('saved');
+      savedPhrases.find();
+    }
+  });
+});
+
+/*TO API - get the word's entry Id*/
+app.post('/usefulphrases/wordgetid', function(req, res) {
+  /*query Cambridge dictionary*/
+  var baseUrl = 'https://dictionary.cambridge.org/api/v1/';
+  var searchWord = req.body;
+  console.log("******SEARCH WORDS ARE*********");
+  console.log(searchWord);
+  var endpoint = 'dictionaries/british/search/?q='+searchWord+'&pagesize=10&pageindex=1';
+  console.log('QUERY TO API: '+ baseUrl+endpoint);
+
+  request({
+    url: baseUrl+endpoint,
+    headers: {'accesskey': 'mcq9cxUgNQdTw9O6dE7WjIjeB87UCOjd2TRtHW9PTfzCgPgqrvZe8J7uqsQx6Nv8'},
+    method: 'GET',
+    callback: function(error, response, body) {
+      res.send(body);
+    }
+  });
+});
+
+/*TO API  - get the word's definition*/
+app.post('/usefulphrases/wordgetdef', function(req, res) {
+  var baseUrl = 'https://dictionary.cambridge.org/api/v1/';
+  var searchEntry = req.body;
+  var endpoint = 'dictionaries/british/entries/'+searchEntry+'/?format=html';
+  console.log('QUERY TO API FOR ENTRIES: '+ baseUrl+endpoint);
+
+  request ({
+    url: baseUrl+endpoint,
+    headers: {'accesskey': 'mcq9cxUgNQdTw9O6dE7WjIjeB87UCOjd2TRtHW9PTfzCgPgqrvZe8J7uqsQx6Nv8'},
+    method: 'GET',
+    callback: function(error, response, body) {
+      res.send(body);
+    }
+  });
+});
+
+/* save phrase */
+app.post('/usefulphrases/new', function(req,res) {
+  console.log("request.body: " + req.body)
+  var savedPhrase = req.body;
+
+  MongoClient.connect(mongoUrl, function(err, db){
+    var savedCollection = db.collection('saved');
+    if (err) {
+      console.log("Unable to connect to the mongodb server. Error: ", err);
+    } else {
+        console.log("connected to server. saving....")
+        console.log(req.body);
+
+        /* insert */
+        savedCollection.insert([savedPhrase], function(err, result){
+          if (err){
+            console.log(err);
+            response.json("error");
+          } else {
+            console.log("inserted.");
+            console.log('Result: ', result);
+            console.log("end result");
+            response.json(result);
+          }
+        });
+    }
+  });
+});
+
+/***port listening***/
+app.listen(PORT, function() {
+  console.log('App is listening on port'+ PORT);
 });
